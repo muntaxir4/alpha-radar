84f9449202cbfdbda872f34f059d235edad1c911
muntaxir4 <mjmullick0608@gmail.com>
2024-06-13 21:06:07 +0530

    Add Tilt Profile Card

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,15 +2,18 @@
 import Search from "./Search"
 import ProfileCard from "./ProfileCard"
 import DisplayOthers from "./DisplayOthers";
+import Tilt from "react-parallax-tilt";
 
 function App() {
 
   return <div className="text-center">
     <h1 className="text-3xl font-bold m-4 underline">Alpha Radar</h1>
-    <Search />
-    <div className="w-[90%] sm:max-w-[40%] h-96 mx-auto">
-      <ProfileCard />
-    </div>
+    <Search />   
+    <Tilt>
+      <div className="w-[90%] sm:max-w-[40%] h-96 mx-auto">
+        <ProfileCard />
+      </div>
+    </Tilt>
     <div className="flex flex-col w-11/12 sm:w-3/4 mx-auto">
      <DisplayOthers />
     </div>
